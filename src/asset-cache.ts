0be83eb9aa4b42c8c1e2a255180026bf44059278
Jinwoo Lee <jinwoo68@gmail.com>
2017-10-12 15:45:47 -0700

    Initial commit

---
+++ src/asset-cache.ts
@@ -0,0 +1,82 @@
+import * as http2 from 'http2';
+
+const EMPTY_ASSETS: ReadonlyArray<string> = Object.freeze([]);
+
+type RelatedPaths = Set<string>;
+
+export interface AssetCacheConfig {
+  warmupDuration: number;
+  promotionRatio: number;
+  demotionRatio: number;
+  minimumRequests: number;
+}
+
+interface WarmingMetricsEntry {
+  successes: number;
+  total: number;
+  readonly paths: RelatedPaths;
+}
+
+function setEqual<T>(a: Set<T>, b: Set<T>): boolean {
+  return a.size === b.size && [...a].every(value => b.has(value));
+}
+
+export class AssetCache {
+  private readonly sessionMap: WeakMap<http2.Http2Session, RelatedPaths> =
+      new WeakMap();
+  private readonly warmingMetrics: Map<string, WarmingMetricsEntry> = new Map();
+  private readonly assetMap: Map<string, RelatedPaths> = new Map();
+
+  constructor(private readonly config: AssetCacheConfig) {}
+
+  recordRequestPath(
+      session: http2.Http2Session, path: string, isStatic: boolean): void {
+    if (this.assetMap.has(path)) return;
+
+    const entry = this.sessionMap.get(session);
+    if (!entry) {
+      this.sessionMap.set(session, new Set());
+      session.setTimeout(
+          this.config.warmupDuration, () => this.onWarm(path, session));
+    } else if (isStatic) {
+      // Only static resources are auto-pushed.
+      entry.add(path);
+    }
+  }
+
+  getAssetsForPath(path: string): Set<string> {
+    const assets = this.assetMap.get(path);
+    return new Set<string>(assets || EMPTY_ASSETS);
+  }
+
+  private onWarm(path: string, session: http2.Http2Session): void {
+    if (this.assetMap.has(path)) return;
+
+    const sessionMapEntry = this.sessionMap.get(session);
+    if (sessionMapEntry === undefined) {
+      console.warn('Session does not exist. Already deleted?');
+      return;
+    }
+
+    let warmingMetricsEntry = this.warmingMetrics.get(path);
+    if (warmingMetricsEntry === undefined) {
+      warmingMetricsEntry = {successes: 0, total: 0, paths: sessionMapEntry};
+      this.warmingMetrics.set(path, warmingMetricsEntry);
+    }
+    if (setEqual(sessionMapEntry, warmingMetricsEntry.paths)) {
+      warmingMetricsEntry.successes++;
+    }
+    warmingMetricsEntry.total++;
+    if (warmingMetricsEntry.total < this.config.minimumRequests) return;
+
+    const ratio = warmingMetricsEntry.successes / warmingMetricsEntry.total;
+    if (ratio >= this.config.promotionRatio) {
+      this.assetMap.set(path, warmingMetricsEntry.paths);
+      this.warmingMetrics.delete(path);
+    } else if (ratio <= this.config.demotionRatio) {
+      // Try again with the current set of paths, this may be brittle
+      this.warmingMetrics.set(
+          path, {successes: 1, total: 1, paths: sessionMapEntry});
+    }
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as express from 'express';
+import * as fs from 'fs';
+import * as http2 from 'http2';
+import * as path from 'path';
+
+import {AssetCache} from './asset-cache';
+import {AssetCacheConfig} from './asset-cache';
+
+export {AssetCacheConfig} from './asset-cache';
+
+type Request = express.Request&http2.Http2ServerRequest;
+type Response = express.Response&http2.Http2ServerResponse;
+
+const DEFAULT_CACHE_CONFIG: AssetCacheConfig = {
+  warmupDuration: 500,
+  promotionRatio: 0.8,
+  demotionRatio: 0.2,
+  minimumRequests: 1,
+};
+
+export class AutoPush {
+  private readonly assetCache: AssetCache;
+
+  constructor(cacheConfig: AssetCacheConfig = DEFAULT_CACHE_CONFIG) {
+    this.assetCache = new AssetCache(cacheConfig);
+  }
+
+  static(root: string): express.RequestHandler {
+    return (req: Request, res: Response, next: express.NextFunction): void => {
+      const reqPath = req.path;
+      const stream = req.stream;
+      // TODO(jinwoo): Remove type casting once @types/node is fixed.
+      stream.respondWithFile(path.join(root, reqPath), undefined, {
+        getTrailers: (trailers) => {
+          // Piggy-back on getTrailers() to record this path as a static
+          // resource.
+          this.assetCache.recordRequestPath(stream.session, reqPath, true);
+        },
+        onError: (err: Error) => {
+          // Not a valid file. Record this path as a non-static resource.
+          this.assetCache.recordRequestPath(stream.session, reqPath, false);
+          next();
+        },
+      } as http2.ServerStreamFileResponseOptions);
+
+      // Auto-push related assets.
+      for (const asset of this.assetCache.getAssetsForPath(reqPath)) {
+        stream.pushStream({':path': asset}, pushStream => {
+          pushStream.respondWithFile(path.join(root, asset));
+        });
+      }
+    };
+  }
+}
